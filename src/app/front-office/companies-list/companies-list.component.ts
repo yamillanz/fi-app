f2705169007668427f0e305b968aa27183982c97
yamil.lanz <yamil.lanz@externos-cl.cencosud.com>
2024-11-11 13:55:47 -0400

    add crud methods

--- src/app/front-office/companies-list/companies-list.component.ts
+++ src/app/front-office/companies-list/companies-list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { CompaniesService } from './services/companies.service';
@@ -11,44 +11,24 @@ import { CompaniesService } from './services/companies.service';
   templateUrl: './companies-list.component.html',
   styleUrl: './companies-list.component.scss',
 })
-export class CompaniesListComponent {
+export class CompaniesListComponent implements OnInit {
   companiesServ = inject(CompaniesService);
-  companies = [
-    {
-      name: 'Empresa 1',
-      rif: 'J-12345678-9',
-      address: 'Dirección 1',
-      phone: '1234567890',
-    },
-    {
-      name: 'Empresa 2',
-      rif: 'J-98765432-1',
-      address: 'Dirección 2',
-      phone: '0987654321',
-    },
-    {
-      name: 'Empresa 3',
-      rif: 'J-56789012-3',
-      address: 'Dirección 3',
-      phone: '3456789012',
-    },
-    {
-      name: 'Empresa 4',
-      rif: 'J-21098765-4',
-      address: 'Dirección 4',
-      phone: '6789012345',
-    },
-    {
-      name: 'Empresa 5',
-      rif: 'J-43210987-6',
-      address: 'Dirección 5',
-      phone: '9012345678',
-    },
-  ];
+  companies: any[] = [];
 
   constructor() {
-    this.companiesServ.getCompanies();
+    // ...existing code...
   }
+
+  ngOnInit() {
+    this.loadCompanies();
+  }
+
+  async loadCompanies() {
+    const companiesData = await this.companiesServ.getCompanies();
+    // this.companies = companiesData ? Object.values(companiesData) : [];
+    this.companies = companiesData ?? [];
+  }
+
   editing: boolean = false;
 
   onRowEditInit(company: any) {}
